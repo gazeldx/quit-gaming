4462a66334765c79734a9d03e7f243d74ec12827
Lane Zhang <zjloveztt@gmail.com>
2024-05-28 19:28:57 +0800

    Added `zh.yml` and set 'zh' as default locale.

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,8 @@ module QuitGaming
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    #
+    config.i18n.default_locale = :zh
 
     config.autoload_paths += Dir["#{Rails.root}/lib"]
   end
