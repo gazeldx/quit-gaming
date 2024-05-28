1a4d292cb9bb30bb0ef6274358921e76f44f70d6
Lane Zhang <zjloveztt@gmail.com>
2024-05-28 19:27:53 +0800

    Remove `body` from Article; Merged two migration into one.

--- db/migrate/20240524074034_add_path_to_articles.rb
+++
@@ -1,5 +0,0 @@
-class AddPathToArticles < ActiveRecord::Migration[7.1]
-  def change
-    add_column :articles, :path, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_24_074034) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_15_110738) do
   create_table "articles", force: :cascade do |t|
+    t.string "path"
     t.string "title"
-    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "path"
   end
 
 end
