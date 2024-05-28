78dd5c4056560f1111302fbbc4feb3579b555be9
Lane Zhang <zjloveztt@gmail.com>
2024-05-28 20:26:19 +0800

    Added db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+Dir['_articles/*.md'].each do |file_path|
+  id_path = file_path.sub('_articles/', '').sub('.md', '').split('_')
+  Article.find_or_create_by!(id: id_path[0]) do |article|
+    article.path = id_path[1]
+  end
+end
